7c5161705d1897ba61b4ec361c849292d5a3180b
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2022-10-09 11:54:01 -0500

    commit tree dfe458d844f0c9e9d58f85442f2cf18071b5f5ec

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'Male', 'Brown', 'Happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'Male', 'Blonde', 'Mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'Female', 'Black', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'Male', 'Brown', 'Stoic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'Female', 'Black', 'Sleepy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 11, 'Male', 'Brown', 'Cranky', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'Brown', 'Hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, 'Male', 'Brown', 'Excited', 1);
